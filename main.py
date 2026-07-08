9360ea397a0d9f210458d0c1d5016eb9b42088ef
henrryribeiro <henrry.marcell@gmail.com>
2026-07-08 14:37:13 -0300

    add excel reader

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from urllib3.util import url
+from src.downloader import download_file
+from src.excel_reader import read_excel
 
-from src.downloader import download
 
-url = "https://docs.google.com/spreadsheets/d/1FJeNr6pSydj4dNNmjklWWDsdUiCo55Uw/export?format=xlsx"
+spreadsheet_url = (
+    "https://docs.google.com/spreadsheets/d/"
+    "1FJeNr6pSydj4dNNmjklWWDsdUiCo55Uw/"
+    "export?format=xlsx"
+)
 
-download(
-    url,
-    "files/planilha.xlsx"
+
+file_path = "files/planilha.xlsx"
+
+
+download_file(
+    spreadsheet_url,
+    file_path
+)
+
+
+loan_requests = read_excel(
+    file_path
+)
+
+
+print(
+    f"{len(loan_requests)} solicitações carregadas."
 )
